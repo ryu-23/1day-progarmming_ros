4118f1d7bd4c2c5a5953d8095b175ad5e11b1885
ryu-23 <85020119+ryu-23@users.noreply.github.com>
2022-09-24 20:58:27 +0900

    Add files via upload

---
+++ 20220921/Motor_control_pwm.h
@@ -0,0 +1,14 @@
+/*
+ * Motor_control_pwm.h
+ *
+ *  Created on: 2022. 8. 4.
+ *      Author: user
+ */
+
+#ifndef MOTOR_CONTROL_PWM_H_
+#define MOTOR_CONTROL_PWM_H_
+
+void L298_Pin_init(void);
+void motor_control(int pwm);
+
+#endif /* MOTOR_CONTROL_PWM_H_ */
