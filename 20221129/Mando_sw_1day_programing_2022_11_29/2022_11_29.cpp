9756a3d577accf93031043ed982461aea84638eb
ryu-23 <85020119+ryu-23@users.noreply.github.com>
2022-11-30 17:44:06 +0900

    Update 2022_11_29.cpp

--- 20221129/Mando_sw_1day_programing_2022_11_29/2022_11_29.cpp
+++ 20221129/Mando_sw_1day_programing_2022_11_29/2022_11_29.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    int Max_rpm = 3000;
     int num1, num2;
     float rpm, pulse, hz, total_pulse ,min_period;
     printf("insert rpm : ");
@@ -13,16 +14,14 @@ int main()
     
     rpm = num1;
     pulse = num2;
-    if (rpm <= 3000) {
+    rpm = rpm >= Max_rpm ? Max_rpm : rpm;
         hz = rpm * (1.0 / 60.0);
         total_pulse = hz * pulse;
         min_period = 1 / total_pulse * 1000;
 
         printf("hz = %0.2f Hz \n", hz);
         printf("min_period  = %0.2f msec \n", min_period);
-    }
-    else {
-        printf("false");
-    }
+    
+    
     return 0;
 }
