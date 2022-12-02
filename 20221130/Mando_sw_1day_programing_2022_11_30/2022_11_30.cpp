62dafc2502469a1809cbc9a289de982f1038e1f1
ryu-23 <85020119+ryu-23@users.noreply.github.com>
2022-12-02 19:25:53 +0900

    Update 2022_11_30.cpp

--- 20221130/Mando_sw_1day_programing_2022_11_30/2022_11_30.cpp
+++ 20221130/Mando_sw_1day_programing_2022_11_30/2022_11_30.cpp
@@ -3,24 +3,26 @@
 
 int main()
 {
-    int num1, num2;
+    int num1 = 0, num2;
     float BPS, byte, total_time, one_time;
-    printf("insert BPS : ");
+    float BPS_select[] = {1200,2400,4800,9600,14400,19200,38400,57600,115200,230400,460800,921600};
+    printf("-------------------SELECT_BPS---------------------\n");
+    printf("1:1200 2:2400 3:4800 4:9600 5: 14400 6:19200  \n7:38400 8:57600 9:115200 10:230400 11:460800 12:921600\n");
+    printf("--------------------------------------------------\n");
+    printf("select BPS : ");
     scanf("%d", &num1);
     printf("insert byte : ");
     scanf("%d", &num2);
 
-    BPS = num1;
+    BPS = BPS_select[num1-1];
     byte = num2;
     
     total_time = (byte / BPS) *1000;
     one_time = total_time / byte;
 
+    printf("\nselect_BPS = (%d)%0.0f  \n", num1,BPS);
     printf("total time = %f msec \n", total_time);
-    printf("one_time = %f msec", one_time);
-
-    //T = A/S A = 8byte S = 19200 B/s
-    //  8B/19200B/s = T
+    printf("one_time = %f msec\n", one_time);
 
     return 0;
 }
