9539a8f6f9f3410b151201dad87e8ae25783998d
ryu-23 <85020119+ryu-23@users.noreply.github.com>
2023-01-02 17:47:08 +0900

    Add files via upload

---
+++ 20230102_ino -GUI/GUI_TEST1.py
@@ -0,0 +1,108 @@
+## Ex 5-1. QPushButton.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QCheckBox, QLineEdit
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        label1 = QLabel('PORPOSE', self)
+        label1.move(120, 60)
+
+        label2 = QLabel('MAKE MOTOE CONTROLLER', self)
+        label2.move(100, 100)
+
+        label3 = QLabel('P', self)
+        label3.move(30, 250)
+
+        label4 = QLabel('PI', self)
+        label4.move(120, 250)
+
+        label5 = QLabel('PD', self)
+        label5.move(210, 250)
+
+        font1 = label1.font()
+        font1.setPointSize(20)
+        C:\TensorFlow2
+        .0\pythonProject\GUI_TEST1.py
+        font2 = label2.font()
+        font2.setFamily('Times New Roman')
+        font2.setBold(True)
+
+        label1.setFont(font1)
+        label2.setFont(font2)
+
+        btn1 = QPushButton('&P', self)
+        btn1.move(30, 150)
+
+
+
+        btn2 = QPushButton(self)
+        btn2.move(30, 180)
+        btn2.setText('PI&')
+
+        btn3 = QPushButton('PD', self)
+        btn3.move(30, 210)
+        btn3.setEnabled(True)
+
+
+        cb = QCheckBox('Show title', self)
+        cb.move(10, 10)
+        cb.toggle()
+        cb.stateChanged.connect(self.changeTitle)
+
+        self.lbl = QLabel(self)
+        self.lbl.move(60, 250)
+
+        qle = QLineEdit(self)
+        qle.move(110, 150)
+        qle.textChanged[str].connect(self.onChanged)
+
+        self.lbl2 = QLabel(self)
+        self.lbl2.move(150, 250)
+
+        qle2 = QLineEdit(self)
+        qle2.move(110, 180)
+        qle2.textChanged[str].connect(self.onChanged2)
+
+        self.lbl3 = QLabel(self)
+        self.lbl3.move(240, 250)
+
+        qle3 = QLineEdit(self)
+        qle3.move(110, 210)
+        qle3.textChanged[str].connect(self.onChanged3)
+
+        self.setWindowTitle('MOTOE CONTROLLER')
+        self.setGeometry(350, 350, 350, 350)
+        self.show()
+
+    def changeTitle(self, state):
+        if state == Qt.Checked:
+            self.setWindowTitle('MOTOE CONTROLLER')
+        else:
+            self.setWindowTitle(' ')
+
+    def onChanged(self, text1):
+        self.lbl.setText(text1)
+        self.lbl.adjustSize()
+
+    def onChanged2(self, text):
+        self.lbl2.setText(text)
+        self.lbl2.adjustSize()
+
+    def onChanged3(self, text):
+        self.lbl3.setText(text)
+        self.lbl3.adjustSize()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
